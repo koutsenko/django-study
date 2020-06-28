375528f6727940f0006ed03d87955dd19e6d125c
kds <koutsenko.work@gmail.com>
2020-06-29 00:14:30 +0300

    An example of model modifying

--- mysite/web/models.py
+++ mysite/web/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 class Publication(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=256)
     text = models.TextField()
     date = models.DateTimeField(default=datetime.now)
